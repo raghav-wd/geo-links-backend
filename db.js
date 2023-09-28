cdc5ea9bb85d546563eed2ac503dd218c00327e9
rishi22rs <rishisrivastava22rs@gmail.com>
2023-09-28 18:46:28 +0530

    added server credentials

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
+
+// var db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   database: "geo-links"
+// });
 
 var db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "geo-links"
+  host: "sql12.freemysqlhosting.net",
+  user: "sql12649713",
+  password: "ZxUy1z7lrX",
+  database: "sql12649713",
+  post: 3306,
 });
 
-db.connect(function(err) {
+db.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
